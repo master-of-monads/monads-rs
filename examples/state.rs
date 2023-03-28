59fbdc48619c14d1dfbeeb3ba7cb97582af21b4b
Joakim <joakimanderlind@gmail.com>
2023-03-28 16:49:45 +0200

    Added State Monad and Example

---
+++ examples/state.rs
@@ -0,0 +1,152 @@
+use std::{borrow::Borrow, fmt};
+
+use monads_rs::*;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Player {
+	CROSS,
+	NOUGHT,
+}
+
+impl Player {
+	fn flip(&self) -> Self {
+		if *self == Player::CROSS {
+			return Player::NOUGHT;
+		} else {
+			return Player::CROSS;
+		}
+	}
+}
+
+impl fmt::Display for Player {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Player::CROSS => write!(f, "X"),
+			Player::NOUGHT => write!(f, "O"),
+		}
+	}
+}
+
+fn disp(op: Option<Player>) -> String {
+	format!("{}", op.map_or("_".to_string(), |x| x.to_string()))
+}
+
+type Board = [Option<Player>; 9];
+
+#[derive(Clone, Copy, Debug)]
+struct GameState {
+	board: Board,
+	turn: Player,
+	round_count: usize,
+}
+
+impl GameState {
+	pub fn new() -> Self {
+		Self {
+			board: [None, None, None, None, None, None, None, None, None],
+			turn: Player::CROSS,
+			round_count: 1,
+		}
+	}
+}
+
+impl fmt::Display for GameState {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(
+			f,
+			"{}{}{}\n{}{}{}\n{}{}{}\n",
+			disp(self.board[0]),
+			disp(self.board[1]),
+			disp(self.board[2]),
+			disp(self.board[3]),
+			disp(self.board[4]),
+			disp(self.board[5]),
+			disp(self.board[6]),
+			disp(self.board[7]),
+			disp(self.board[8]),
+		)
+	}
+}
+
+#[monadic]
+fn inc_round_counter() -> State<'static, GameState, ()> {
+	let mut state = State::<'_, GameState, GameState>::get()?;
+	state.round_count += 1;
+	return State::<'_, GameState, ()>::set(state);
+}
+
+// Getters
+#[monadic]
+fn get_board() -> State<'static, GameState, Board> {
+	let state = State::<'_, GameState, GameState>::get()?;
+	State::ret(state.board)
+}
+
+#[monadic]
+fn get_current_player() -> State<'static, GameState, Player> {
+	let state = State::<'_, GameState, GameState>::get()?;
+	State::ret(state.turn)
+}
+
+#[monadic]
+fn get_turn_counter() -> State<'static, GameState, usize> {
+	let state = State::<'_, GameState, GameState>::get()?;
+	State::ret(state.round_count)
+}
+// Setters
+
+#[monadic]
+fn set_board(board: Board) -> State<'static, GameState, ()> {
+	State::<'_, GameState, GameState>::get().bind(move |mut state| {
+		state.board = board;
+		State::<'_, GameState, ()>::set(state)
+	})
+}
+
+#[monadic]
+fn switch_current_player() -> State<'static, GameState, ()> {
+	let mut state = State::<'_, GameState, GameState>::get()?;
+	state.turn = state.turn.flip();
+	State::<'_, GameState, ()>::set(state)
+}
+
+#[monadic]
+fn inc_turn_counter(board: Board) -> State<'static, GameState, ()> {
+	let mut state = State::<'_, GameState, GameState>::get()?;
+	state.round_count += 1;
+	State::<'_, GameState, ()>::set(state)
+}
+
+#[monadic]
+fn set_piece(x: usize, y: usize) -> State<'static, GameState, ()> {
+	get_board().bind(move |board| {
+		get_current_player().bind(move |player| {
+			let mut new_board = board.clone();
+			new_board[x % 3 + y * 3] = Some(player);
+			set_board(new_board)
+		})
+	})
+}
+
+#[monadic]
+fn play_turn(x: usize, y: usize) -> State<'static, GameState, ()> {
+	set_piece(x, y)?;
+	switch_current_player()?;
+	inc_round_counter()
+}
+
+#[monadic]
+fn program() -> State<'static, GameState, ()> {
+	play_turn(0, 0)?;
+	play_turn(0, 2)?;
+	play_turn(2, 0)?;
+	play_turn(1, 0)?;
+	play_turn(2, 2)?;
+	play_turn(2, 1)?;
+	play_turn(1, 1)
+}
+
+fn main() {
+	let state = program().run_state.as_ref()(GameState::new());
+	println!("{}", state.0);
+}
--- src/state.rs
+++ src/state.rs
@@ -1,19 +1,62 @@
-use super::{Applicative, Functor, Monad};
-use std::{rc::Rc, marker::PhantomData};
-
+use super::{Functor, Monad};
+use std::rc::Rc;
 
 #[derive(Clone)]
-struct State<'a, S,V> {
-    runState: &'a (dyn Fn(S) -> V),
+pub struct State<'a, S, V> {
+	pub run_state: Rc<dyn Fn(S) -> (S, V) + 'a>,
 }
 
-impl<'a,S:'a,A:'a> Functor<'a,A> for State<S,A> {
-	type Map<B> = State<S,B>;
-
-	fn map<B, F: Fn(A) -> B>(self, f: &'static F) -> Self::Map<B> {
-        let rs = self.runState.clone();
-        let new_rs = move |s| f(rs.as_ref()(s));
-        return Self::Map::<B>{runState : Rc::new(new_rs)}
+impl<'a, S: Clone + 'a, V> State<'a, S, V> {
+	pub fn get() -> State<'a, S, S> {
+		let f = move |s: S| (s.clone(), s);
+		State {
+			run_state: Rc::new(f),
+		}
+	}
+	pub fn set(s: S) -> State<'a, S, ()> {
+		let f = move |_| (s.clone(), ());
+		State {
+			run_state: Rc::new(f),
+		}
 	}
 }
 
+impl<'a, S: 'a, A: 'a> Functor<'a, A> for State<'a, S, A> {
+	type Target<B> = State<'a, S, B>;
+	fn fmap<B, F>(self, f: F) -> Self::Target<B>
+	where
+		F: Fn(A) -> B + 'a,
+	{
+		let new_f = move |s| {
+			let (new_state, new_value) = self.run_state.as_ref()(s);
+			(new_state, f(new_value))
+		};
+
+		State {
+			run_state: Rc::new(new_f),
+		}
+	}
+}
+
+impl<'c, S: 'c, C: 'c + Clone> Monad<'c, C> for State<'c, S, C> {
+	type Bind<B> = State<'c, S, B>;
+
+	/// Sequentially compose two actions, passing any value produced by the
+	/// first as an argument to the second.
+	fn bind<B, F: Fn(C) -> Self::Bind<B> + 'c>(self, f: F) -> Self::Bind<B> {
+		let new_f = move |s| {
+			let (sp, vp) = self.run_state.as_ref()(s);
+			let m = f(vp);
+			return m.run_state.as_ref()(sp);
+		};
+		State {
+			run_state: Rc::new(new_f),
+		}
+	}
+	fn ret(c: C) -> Self {
+		let f = move |s| (s, c.clone());
+		State {
+			run_state: Rc::new(f),
+		}
+	}
+}
