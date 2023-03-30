043c1c0122e41e79d70525ff481fc0275e4c72cf
Joakim <joakimanderlind@gmail.com>
2023-03-30 14:17:47 +0200

    increase calls in option benchmark to display perfomance cost of multiple binds

--- benches/option_benchmark.rs
+++ benches/option_benchmark.rs
@@ -1,52 +1,64 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{
+	black_box, criterion_group, criterion_main, BenchmarkId, Criterion,
+};
 use monads_rs::*;
 
-fn is_even(v : u8) -> Option<u8> {
-    if v % 2 == 0 {
-        return Some(v);
-    }
-    else {
-        return None;
-    }
+fn is_even(v: u8) -> Option<u8> {
+	if v % 2 == 0 {
+		return Some(v);
+	} else {
+		return None;
+	}
 }
 
 #[monadic]
-fn monadic(v : u8) -> Option<u8> {
-    let mut a = 0;
-    a += is_even(v)?;
-    a += is_even(v-2)?;
-    a += is_even(v-4)?;
-    a += is_even(v-8)?;
-    a += is_even(v-16)?;
-    a += is_even(v-32)?;
-    a += is_even(v-64)?;
-    a += is_even(v-128)?;
-    a += is_even(v-10)?;
-    a += is_even(v-12)?;
-    Some(a)
+fn monadic(v: u8) -> Option<u8> {
+	let a = is_even(v)?;
+	let b = is_even(v)?;
+	let c = is_even(v)?;
+	let result = is_even(a + b + c)?;
+	let a = is_even(v)?;
+	let b = is_even(v)?;
+	let c = is_even(v)?;
+	let result = is_even(a + b + c)?;
+	let a = is_even(v)?;
+	let b = is_even(v)?;
+	let c = is_even(v)?;
+	let result = is_even(a + b + c)?;
+	Option::ret(result)
 }
 
-fn non_monadic(v : u8) -> Option<u8> {
-    let mut a = 0;
-    a += is_even(v)?;
-    a += is_even(v-2)?;
-    a += is_even(v-4)?;
-    a += is_even(v-8)?;
-    a += is_even(v-16)?;
-    a += is_even(v-32)?;
-    a += is_even(v-64)?;
-    a += is_even(v-128)?;
-    a += is_even(v-10)?;
-    a += is_even(v-12)?;
-    Some(a)
+fn non_monadic(v: u8) -> Option<u8> {
+	let a = is_even(v)?;
+	let b = is_even(v)?;
+	let c = is_even(v)?;
+	let result = is_even(a + b + c)?;
+	let a = is_even(v)?;
+	let b = is_even(v)?;
+	let c = is_even(v)?;
+	let result = is_even(a + b + c)?;
+	let a = is_even(v)?;
+	let b = is_even(v)?;
+	let c = is_even(v)?;
+	let result = is_even(a + b + c)?;
+	Option::ret(result)
 }
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Option");
-    for i in [250].iter() {
-        group.bench_with_input(BenchmarkId::new("Monadic Option",i), i, |b,i| b.iter(|| monadic(black_box(*i))));
-        group.bench_with_input(BenchmarkId::new("Refrence Option",i), i, |b,i| b.iter(|| non_monadic(black_box(*i))));
-    }
-    group.finish();
+	let mut group = c.benchmark_group("Option");
+	for i in [250].iter() {
+		group.bench_with_input(
+			BenchmarkId::new("Monadic Option", i),
+			i,
+			|b, i| b.iter(|| monadic(black_box(*i))),
+		);
+		group.bench_with_input(
+			BenchmarkId::new("Refrence Option", i),
+			i,
+			|b, i| b.iter(|| non_monadic(black_box(*i))),
+		);
+	}
+	group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
