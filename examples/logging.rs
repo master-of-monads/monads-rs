085bf4bcd1c2df7445f94688ba97a31ec13adb64
Mårten Åsberg <marten.asberg@outlook.com>
2023-04-17 14:32:17 +0200

    Fix Logging example
    Also fixed logic error from before

--- examples/logging.rs
+++ examples/logging.rs
@@ -1,5 +1,8 @@
 #![cfg(feature = "logging")]
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use monads_rs::logging::Logging;
 use monads_rs::*;
 
@@ -20,10 +23,12 @@ fn startup_nuclear_reactor() -> Logging<bool> {
 	let core_temp = get_core_temp()?;
 
 	if core_temp < 200.0 {
-		Logging::log("Reactor core temperature is nominal")?;
+		Logging::log(format!(
+			"Reactor core temperature is nominal ({core_temp})"
+		))?;
 		Logging::ret(true)
 	} else {
-		Logging::log("Reactor core is too hot, abort")?;
+		Logging::log(format!("Reactor core is too hot, abort ({core_temp})"))?;
 		Logging::ret(false)
 	}
 }
@@ -43,14 +48,15 @@ fn activate_warning_lights() -> Logging<()> {
 
 #[monadic]
 fn get_core_temp() -> Logging<f32> {
-	let mut core_temp_arg = 0.0;
-	for i in 1..=3 {
-		core_temp_arg += 40.0 + (i as f32);
+	let core_temp_arg = Rc::new(RefCell::new(0.0));
+	let core_temp_arg_clone = core_temp_arg.clone();
+	for i in 0..4 {
+		*core_temp_arg_clone.borrow_mut() += 40.0 + (i as f32);
 		Logging::log(format!(
 			"Reading core temperature sensor #{0}: {1}c",
 			i,
 			40.0 + (i as f32)
 		))?;
 	}?;
-	Logging::ret(core_temp_arg / 4.0)
+	Logging::ret(*core_temp_arg.borrow() / 4.0)
 }
