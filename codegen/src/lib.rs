ede4a78a725ee032fb1a514a503affae7499f5b4
Joakim <joakimanderlind@gmail.com>
2023-04-21 15:15:22 +0200

    Print error when using explicit returns

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -7,7 +7,7 @@ mod syntax_errors;
 use blocks::bind_in_block;
 use quote::ToTokens;
 use syn::{parse_macro_input, ItemFn};
-use syntax_errors::report_syntax_errors;
+use syntax_errors::{report_syntax_prior, report_syntax_after};
 
 /// An attribute for writing monadic functions using the `?` operator as a
 /// do-notation style binding operator.
@@ -17,8 +17,9 @@ pub fn monadic(
 	item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
 	let function = parse_macro_input!(item as ItemFn);
+	let function = report_syntax_prior(function);
 	let function = convert_monadic_binds(function);
-	let function = report_syntax_errors(function);
+	let function = report_syntax_after(function);
 	function.into_token_stream().into()
 }
 
