3d5b0835c30388ec8c67d53bd19309006e979df9
Joakim <joakimanderlind@gmail.com>
2023-04-21 15:29:38 +0200

    Add error suffix to error reporting plugins

--- codegen/src/syntax_errors/mod.rs
+++ codegen/src/syntax_errors/mod.rs
@@ -1,13 +1,13 @@
 use syn::ItemFn;
 
-mod unhandled_binds;
 mod early_return;
+mod unhandled_binds;
 
 /// Reports all monadic related syntax errors
-pub(crate) fn report_syntax_after(function: ItemFn) -> ItemFn {
+pub(crate) fn report_syntax_errors_after(function: ItemFn) -> ItemFn {
 	unhandled_binds::report_error_for_binds(function)
 }
 
-pub(crate) fn report_syntax_prior(function: ItemFn) -> ItemFn {
+pub(crate) fn report_syntax_errors_prior(function: ItemFn) -> ItemFn {
 	early_return::report_error_for_early_return(function)
 }
